71a22ecb7580de38661f176413f435223e8bf3bf
jiangKlijna <leil729564090@outlook.com>
2016-05-23 21:05:47 +0800

    :boom:update_AppUtil

--- library/src/main/java/com/jiangKlijna/application/AppUtil.java
+++ library/src/main/java/com/jiangKlijna/application/AppUtil.java
@@ -2,12 +2,19 @@ package com.jiangKlijna.application;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+import android.view.inputmethod.InputMethodManager;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -116,11 +123,99 @@ public class AppUtil {
     }
 
     /**
+     * 判断当前网络是否是移动数据网络.
+     *
+     * @param context the context
+     * @return boolean
+     */
+    public static boolean isMobile(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        return (activeNetInfo != null && activeNetInfo.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+
+    /**
+     * wifi是否打开.
+     *
+     * @param context the context
+     * @return true, if is wifi enabled
+     */
+
+    public static boolean isWifiEnabled(Context context) {
+        ConnectivityManager mgrConn = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        TelephonyManager mgrTel = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        return ((mgrConn.getActiveNetworkInfo() != null && mgrConn
+                .getActiveNetworkInfo().getState() == NetworkInfo.State.CONNECTED) || mgrTel
+                .getNetworkType() == TelephonyManager.NETWORK_TYPE_UMTS);
+    }
+
+    /**
      * 是否连接Wifi
      */
     public static boolean isConnWifi(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo wifiNetworkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         return wifiNetworkInfo != null && wifiNetworkInfo.isConnected();
+    }
+
+    /**
+     * Gps是否打开
+     * 需要<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />权限
+     *
+     * @param context the context
+     * @return true, if is gps enabled
+     */
+    public static boolean isGpsEnabled(Context context) {
+        LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    /**
+     * 获取屏幕尺寸与密度.
+     * DisplayMetrics{density=1.5, width=480, height=854, scaledDensity=1.5, xdpi=160.421, ydpi=159.497}
+     * DisplayMetrics{density=2.0, width=720, height=1280, scaledDensity=2.0, xdpi=160.42105, ydpi=160.15764}
+     *
+     * @return mDisplayMetrics
+     */
+    public static DisplayMetrics getDisplayMetrics() {
+        return Resources.getSystem().getDisplayMetrics();
+    }
+
+    /**
+     * 描述：打开并安装文件.
+     *
+     * @param context the context
+     * @param file    apk文件路径
+     */
+    public static void installApk(Context context, File file) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+        context.startActivity(intent);
+    }
+
+    /**
+     * 描述：卸载程序.
+     *
+     * @param context     the context
+     * @param packageName 包名
+     */
+    public static void uninstallApk(Context context, String packageName) {
+        Intent intent = new Intent(Intent.ACTION_DELETE);
+        intent.setData(Uri.parse("package:" + packageName));
+        context.startActivity(intent);
+    }
+
+    /**
+     * 打开键盘.
+     *
+     * @param context the context
+     */
+    public static void showSoftInput(Context context) {
+        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
